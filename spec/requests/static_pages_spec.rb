facc784a9a85058e9545f87b953670eab55465a6
sora <hmsubmail1234@gmail.com>
2015-04-15 00:45:18 +0900

    Modify spec by using Shared Example

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,36 +1,35 @@
 require 'spec_helper'
-require 'support/utilities'
 
 describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
-
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
   end
 
   describe "Help page" do
     before { visit help_path }
-
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
-  end
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
+ end
 
   describe "About page" do
     before { visit about_path }
-
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
-  end
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About Us' }
+ end
 
   describe "Contact page" do
     before { visit contact_path }
-
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
-  end
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
+ end
 end
