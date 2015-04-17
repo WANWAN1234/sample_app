4ea6d0b7429830fbb10abd7309edf3c26a469897
sora <hmsubmail1234@gmail.com>
2015-04-18 03:49:37 +0900

    Finish 9.4 9.5

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,6 +9,13 @@ def full_title(page_title)
 	end
 end
 
+def valid_signup
+	fill_in "Name",				with: "Example User"
+	fill_in "Email",			with: "user@example.com"
+	fill_in "Password",			with: "foobar"
+	fill_in "Confirm Password",	with: "foobar"
+end
+
 def valid_signin(user)
 	fill_in "Email",    with: user.email
 	fill_in "Password", with: user.password
